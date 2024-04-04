b53bd8a91e6ff1659339862c869563e099722842
Calebe Apollo <sostenesapollo55@gmail.com>
2024-04-04 15:18:44 -0300

    starting the front

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+// import { useState } from 'react'
+// import { Combobox } from '@headlessui/react'
+
+// const people = [
+//   'Wade Cooper',
+//   'Arlene McCoy',
+//   'Devon Webb',
+//   'Tom Cook',
+//   'Tanya Fox',
+//   'Hellen Schmidt',
+// ]
+
+// function App() {
+//   const [selectedPerson, setSelectedPerson] = useState(people[0])
+//   const [query, setQuery] = useState('')
+
+//   const filteredPeople =
+//     query === ''
+//       ? people
+//       : people.filter((person) => {
+//           return person.toLowerCase().includes(query.toLowerCase())
+//         })
+
+//   return (
+//     <Combobox value={selectedPerson} onChange={setSelectedPerson}>
+//       <Combobox.Input onChange={(event) => setQuery(event.target.value)} />
+//       <Combobox.Options>
+//         {filteredPeople.map((person) => (
+//           <Combobox.Option key={person} value={person}>
+//             {person}
+//           </Combobox.Option>
+//         ))}
+//       </Combobox.Options>
+//     </Combobox>
+//   )
+// }
+
+// export default App;
+
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Paciente from './componentes/Pacient';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <Router>
+
+      <Routes>
+
+      <Route exact path='/'  element={<Paciente/>} />
+      </Routes>
+      {/* <Footer/> */}
+    </Router>
+)}
 
 export default App;
